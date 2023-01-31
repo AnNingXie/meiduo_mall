99fea1633542e112d8e48e1421d1b3423d3aecf7
AnningXie <15003876305@163.com>
2023-01-31 14:44:41 +0800

    1.配置工程日志

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -167,3 +167,55 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+"""
+配置工程日志
+"""
+# 定义三种日志输出格式
+standard_format = '[%(asctime)s][%(threadName)s:%(thread)d][task_id:%(name)s][%(filename)s:%(lineno)d]' \
+                  '[%(levelname)s][%(message)s]' #其中name为getlogger指定的名字
+simple_format = '[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d]%(message)s'
+
+# log配置字典
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {'format': standard_format},
+        'simple': {'format': simple_format},
+    },
+    'filters': {    # 对日志进行过滤
+        'require_debug_true': {     # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        #打印到终端的日志,日志级别等级：CRITICAL > ERROR > WARNING > INFO > DEBUG
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',  # 打印到屏幕
+            'formatter': 'simple'
+        },
+        #打印到文件的日志,收集info及以上的日志
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件
+            'formatter': 'verbose',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs/meiduo.log'),  # 日志文件
+            'maxBytes': 1024*1024*50,  # 日志大小 50M
+            'backupCount': 100,
+            'encoding': 'utf-8',  # 日志文件的编码，再也不用担心中文log乱码了
+        },
+    },
+    'loggers': {    # 日志器
+        #logging.getLogger(__name__)拿到的logger配置
+        'django': {     # 定义一个名为django的日志器
+            'handlers': ['console', 'file'],  # 这里把上面定义的两个handler都加上，即log数据既写入文件又打印到屏幕
+            'level': 'INFO',
+            'propagate': True,  # 向上（更高level的logger）传递
+        },
+    },
+}
