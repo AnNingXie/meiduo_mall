ad0c0d0f3ae82e126b5b0d07a154881d8b40e333
AnningXie <15003876305@163.com>
2023-02-01 17:08:52 +0800

    1.用户注册简单界面 2.用户注册post请求接口

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -4,7 +4,12 @@ from django.views import View
 
 # 用户注册
 class RegisterView(View):
+    
     # 提供用户注册页面
     def get(self, request):
-        return render(request, 'regist.html')
+        return render(request, 'register.html')
+
+    # 实现用户注册逻辑
+    def post(self, request):
         
+        pass
